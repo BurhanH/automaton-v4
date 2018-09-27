ba49517dede1cadbd2bb980d93070764c82e2334
BurhanH <urazalinov@gmail.com>
2018-09-27 18:40:17 -0500

    Added initial test

---
+++ features/step_definition/initial.rb
@@ -0,0 +1,11 @@
+Given(/^we have cucumber installed$/) do
+  puts "Given"
+end
+
+When(/^we implement a test$/) do
+  puts "When"
+end
+
+Then(/^cucumber will test it for us!$/) do
+  puts "Then"
+end
---
+++ features/support/initial.feature
@@ -0,0 +1,6 @@
+Feature: showing off behave
+
+  Scenario: run a simple test
+    Given we have cucumber installed
+    When we implement a test
+    Then cucumber will test it for us!
