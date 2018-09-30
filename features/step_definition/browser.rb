19ef1eb63464d50c4924ba9395141d6461abf85b
BurhanH <urazalinov@gmail.com>
2018-09-30 15:42:47 -0500

    Test 3

--- features/step_definition/browser.rb
+++ features/step_definition/browser.rb
@@ -8,6 +8,6 @@ end
 
 Then(/^resolution is set$/) do
   resolution = @browser.manage.window.size
-  expect(resolution.width).to eq 801
+  expect(resolution.width).to eq 800
   expect(resolution.height).to eq 601
 end
