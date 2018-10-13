bc7f5948fa95c3324b5bcc3fa4bd66f914708f78
BurhanH <urazalinov@gmail.com>
2018-10-13 18:15:01 -0500

    Added a code into the step

--- features/step_definition/google.rb
+++ features/step_definition/google.rb
@@ -3,5 +3,13 @@ When("user goes to google") do
 end
 
 Then("user able to search by term") do
-  # TODO actions
+  # Searching for the input field by name and entering data
+  @browser.find_element(:name, 'q').send_keys 'ruby'
+  # Searching and waiting the search drop-down menu
+  @browser.find_element(:css, '.FPdoLc.VlcLAe').displayed?
+  # Clicking Google Search button
+  @browser.find_element(:css, 'input[name="btnK"]').click
+  # Verifying search results
+  @browser.find_element(:css, 'bNg8Rb').displayed?
+  # TODO needs more verification
 end
