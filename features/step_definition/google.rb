a92debccfc07b43bc2780509b85b2db1b8c3fd06
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-12-30 21:43:49 -0600

    Update google.rb
    Made test more robust

--- features/step_definition/google.rb
+++ features/step_definition/google.rb
@@ -4,11 +4,7 @@ end
 
 Then("user able to search by {string} term") do |target_text|
   # Searching for the input field by name and entering data
-  @browser.find_element(:name, 'q').send_keys target_text
-  # Searching and waiting the search drop-down menu
-  @browser.find_element(:css, '.FPdoLc.VlcLAe').displayed?
-  # Clicking Google Search button
-  @browser.find_element(:css, 'input[name="btnK"]').click
+  @browser.find_element(:name, 'q').send_keys target_text, :enter
   # Verifying search results
   @browser.find_element(:css, '.bNg8Rb').displayed?
   # TODO needs more verification
