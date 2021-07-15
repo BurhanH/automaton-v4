3bbbd2fccc49caffc34a8da59e798ee6f80acea4
Baur Urazalinov <BurhanH@users.noreply.github.com>
2021-07-14 21:14:45 -0500

    Update google.rb

--- features/step_definition/google.rb
+++ features/step_definition/google.rb
@@ -6,6 +6,6 @@ Then("user able to search by {string} term") do |target_text|
   # Searching for the input field by name and entering data
   @browser.find_element(:name, 'q').send_keys target_text, :enter
   # Verifying search results
-  @browser.find_element(:css, '.bNg8Rb').displayed?
+  @browser.find_element(:id, 'search').displayed?
   # TODO needs more verification
 end
